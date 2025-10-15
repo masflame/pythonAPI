7c8afe83d69f2a42329f3e350eba0621db6ade91
NateAphane <99756373+masflame@users.noreply.github.com>
2025-10-15 15:30:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Sample route to process data
+
 @app.route('/process-data', methods=['POST'])
 def process_data():
     # Get the JSON data from the request
     input_data = request.json
     
-    # For example, we can return the input data back to the client
+    # Return message
     result = {
         "message": "Data processed successfully",
         "received_data": input_data
     }
     
-    # Return the result as a JSON response
+    # Returning the result as a JSON response
     return jsonify(result)
 
 if __name__ == '__main__':
